c0d07db4424270cff8f96f313eeffdfbc888dbd9
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-14 15:37:29 +0300

    add Zadacha 034

---
+++ Zadacha034/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве. */
+
+Console.WriteLine("Введите количество чисел в массиве");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = new int[size];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 999);
+}
+Console.WriteLine(string.Join(", ", array));
+int count = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0) count += 1;
+}
+Console.WriteLine($"Количество четных чисел в массиве {count}");
