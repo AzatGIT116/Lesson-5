1fc16bed36abfe8a1ded7e279660e363881fffac
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-14 15:38:11 +0300

    add Zadacha 38

---
+++ Zadacha38/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива. */
+
+Console.WriteLine("Введите количество случайных чисел");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = new int[size];
+Random rand = new Random();
+double max = 0;
+double min = 100;
+for (int i = 0; i < array.Length; i++) array[i] = rand.Next(100);
+for (int i = 0; i < array.Length; i++) 
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+for (int i = 0; i < array.Length; i++) Console.Write(" " + array[i]);
+Console.WriteLine();
+Console.WriteLine("max= " + max);
+Console.WriteLine("min= " + min);
+Console.WriteLine("Разница максимального и минимального:  " + (max - min));
