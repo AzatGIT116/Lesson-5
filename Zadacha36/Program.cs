8a5bdf78f3954c257d29c3f772c8c6920cbdcfcb
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-14 15:37:53 +0300

    add Zadacha 36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+ Console.WriteLine("Введите количество случайных чисел"); 
+int size = int.Parse(Console.ReadLine()!);
+int[] array = new int[size];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 100);
+}
+Console.WriteLine(string.Join(", ", array));
+
+SumOfNumbersOfOddPosition(array);
+void SumOfNumbersOfOddPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum += array[i] ;
+    }
+    Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях равна {sum}");
+}
+ 
